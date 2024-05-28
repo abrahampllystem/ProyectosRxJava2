be992d62e0dd9361512c01341b5ae5f51fc5b282
abrahamperaltallicahua <abrahamperaltallicahua@gmail.com>
2024-05-28 00:28:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProyectosRxJava2
---
+++ src/test/java/pe/com/demo/rxjava2/ProyectosRxJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.demo.rxjava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosRxJava2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
